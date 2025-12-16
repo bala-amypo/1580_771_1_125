3d95742f5eb0b1cd6e4b376e60e394c3de6733c2
bala-amypo <bala@amypo.in>
2025-12-16 10:26:41 +0000

    User commit

--- src/main/java/com/example/demo/entity/Fields.java
+++ src/main/java/com/example/demo/entity/Fields.java
@@ -1,4 +1,15 @@
-public class Fields{
+package com.example.demo.entity;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Fields {
     private long id;
     private String name;
     private String unit;
@@ -6,4 +17,17 @@ public class Fields{
     private boolean active;
     private Timestamp createdAt;
     private Timestamp updatedAt;
+
+    public Fields(){}
+
+    public Fields(boolean active, BigDecimal costPerUnit, Timestamp createdAt, long id, String name, String unit, Timestamp updatedAt) {
+        this.active = active;
+        this.costPerUnit = costPerUnit;
+        this.createdAt = createdAt;
+        this.id = id;
+        this.name = name;
+        this.unit = unit;
+        this.updatedAt = updatedAt;
+    }
+
 }
