4d185e3261001ed7752e9406e550ee1a783325c4
bala-amypo <bala@amypo.in>
2025-12-16 08:16:26 +0000

    User commit

--- src/main/java/com/example/demo/entity/Fields.java
+++ src/main/java/com/example/demo/entity/Fields.java
@@ -1,1 +1,9 @@
-
+public class Fields{
+    private long id;
+    private String name;
+    private String unit;
+    private BigDecimal costPerUnit;
+    private boolean active;
+    private Timestamp createdAt;
+    private Timestamp updatedAt
+}
