84a64a5e716a9c49d151e8d6f926a1ffe1976a10
bala-amypo <bala@amypo.in>
2025-12-19 09:51:01 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/Category.java
@@ -0,0 +1,41 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(
+    name = "categories",
+    uniqueConstraints = @UniqueConstraint(columnNames = "name")
+)
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private String description;
+
+    private boolean active = true;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<MenuItem> menuItems = new HashSet<>();
+
+    public Category() {
+    }
+
+    public Long getId() { return id; }
+    public String getName() { return name; }
+    public String getDescription() { return description; }
+    public boolean isActive() { return active; }
+    public Set<MenuItem> getMenuItems() { return menuItems; }
+
+    public void setId(Long id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+    public void setDescription(String description) { this.description = description; }
+    public void setActive(boolean active) { this.active = active; }
+}
