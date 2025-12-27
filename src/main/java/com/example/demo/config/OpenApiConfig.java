716dbb326628387350baa88e0a914efdacbfb9ad
bala-amypo <bala@amypo.in>
2025-12-27 08:50:49 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -68,7 +68,8 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
 @Configuration
 public class OpenApiConfig {
 
