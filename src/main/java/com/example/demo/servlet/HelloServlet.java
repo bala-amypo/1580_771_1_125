25412f3a9ee08afb3e71717c9315d853f4be3559
bala-amypo <bala@amypo.in>
2025-12-29 18:42:21 +0000

    User commit

--- src/main/java/com/example/demo/servlet/HelloServlet.java
+++ src/main/java/com/example/demo/servlet/HelloServlet.java
@@ -1,33 +1,3 @@
-// package com.example.demo.servlet;
-
-// import jakarta.servlet.ServletException;
-// import jakarta.servlet.annotation.WebServlet;
-// import jakarta.servlet.http.HttpServlet;
-// import jakarta.servlet.http.HttpServletRequest;
-// import jakarta.servlet.http.HttpServletResponse;
-
-// import java.io.IOException;
-// import java.io.PrintWriter;
-
-// @WebServlet(urlPatterns = "/hello-servlet")
-// public class HelloServlet extends HttpServlet {
-
-//     @Override
-//     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-//             throws ServletException, IOException {
-
-//         if (response == null) {
-//             throw new IOException("Response is null");
-//         }
-
-//         response.setContentType("text/plain");
-//         PrintWriter writer = response.getWriter();
-//         writer.write("Hello from servlet");
-//         writer.flush();
-//     }
-// }
-
-
 package com.example.demo.servlet;
 
 import jakarta.servlet.ServletException;
